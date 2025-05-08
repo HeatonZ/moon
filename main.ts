a18a4c14e89f41c4ef8f12147ce4ab6e6f94bda4
Shangfeng Zheng <shangfeng.zheng@xtalpi.com>
2025-05-08 17:29:11 +0800

    update

--- main.ts
+++ main.ts
@@ -1,20 +1,28 @@
 import puppeteer from "npm:puppeteer-core"
 
 const browser = await puppeteer.launch({
-  executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+  executablePath: "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
 });
 const page = await browser.newPage();
 page.setDefaultNavigationTimeout(60000)
 page.on('load',async () => {
     console.log('loadd')
     const items = await page.$$('div[role=listitem]')
-    items.forEach(async item => {
+    
+    const res = await Promise.all(items.map(async item => {
       const titleElement = await item.$('div[data-cy=title-recipe]')
       const title = await titleElement?.evaluate(el => el.innerText);
       const priceElement = await item.$('.a-price span')
       const price = await priceElement?.evaluate(el => el.innerText);
       console.log(title, price)
-    })
+      return {
+        title,
+        price
+      }
+    }))
+    console.log(res)
+    page.close()
+    browser.close()
 })
 // Navigate the page to a URL.
 page.goto('https://www.amazon.sa/s?language=en&me=A325QIOE7W48AL&marketplaceID=A17E79C6D8DWNP');
